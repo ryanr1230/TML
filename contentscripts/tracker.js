b0f8c3caf09ab5bbedf6eed5e52a156acea31ce7
Ryan Rice <ryanr1230@gmail.com>
2016-10-15 14:39:50 -0500

    fixing tracker

--- contentscripts/tracker.js
+++ contentscripts/tracker.js
@@ -1,3 +1,55 @@
-chrome.runtime.sendMessage({url: window.location.href}, function(response) {
-  console.log("did it!");
-});
+// taken from https://www.bennadel.com/blog/1520-binding-events-to-non-dom-objects-with-jquery.htm
+(
+	function( $ ){
+		// Default to the current location.
+		var strLocation = window.location.href;
+		var strHash = window.location.hash;
+		var strPrevLocation = "";
+		var strPrevHash = "";
+		// This is how often we will be checkint for
+		// changes on the location.
+		var intIntervalTime = 100;
+		// This method removes the pound from the hash.
+		var fnCleanHash = function( strHash ){
+			return(
+				strHash.substring( 1, strHash.length )
+				);
+		}
+		// This will be the method that we use to check
+		// changes in the window location.
+		var fnCheckLocation = function(){
+			// Check to see if the location has changed.
+			if (strLocation != window.location.href){
+				// Store the new and previous locations.
+				strPrevLocation = strLocation;
+				strPrevHash = strHash;
+				strLocation = window.location.href;
+				strHash = window.location.hash;
+				// The location has changed. Trigger a
+				// change event on the location object,
+				// passing in the current and previous
+				// location values.
+				$( window.location ).trigger(
+					"change",
+					{
+						currentHref: strLocation,
+						currentHash: fnCleanHash( strHash ),
+						previousHref: strPrevLocation,
+						previousHash: fnCleanHash( strPrevHash )
+					}
+					);
+			}
+		}
+		// Set an interval to check the location changes.
+		setInterval( fnCheckLocation, intIntervalTime );
+	}
+)( jQuery );
+
+
+var sendurl = function() {
+    chrome.runtime.sendMessage({url: window.location.href}, function(response) {
+      console.log("sending " + window.location.href);
+    });
+}
+sendurl();
+$(window.location).on('change', sendurl);
--- extensionscripts/updater.js
+++ extensionscripts/updater.js
@@ -2,7 +2,14 @@ function getRandomInt(min, max) {
         return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+var dont_include = ["https://www.google.com/_/chrome/newtab", "https://www.google.com/webhp?sourceid=chrome-instant"];
+
 chrome.runtime.onMessage.addListener(function(message,sender,sendResponse){
+    for(var i = 0; i < dont_include.length; i++) {
+        if(message.url.startsWith(dont_include[i])) {
+            return;
+        }
+    }
     let access_token = "787158601265668097-tydRuRHmWbgioNpmxYjcNhYHdfau0oU";  
     let consumer_secret = "3piDTzHnxSrZU13XVe9XeplJSp37QwGxDZh4O1bQYSpAyD3OLA";
     let token_secret = "HSHRPRy6v4q7hLjDnFIBMTMXxxhkY4TcMZo618vHl4vSt";
@@ -11,10 +18,12 @@ chrome.runtime.onMessage.addListener(function(message,sender,sendResponse){
     var cb = new Codebird;
     cb.setConsumerKey(consumer_key, consumer_secret);
     cb.setToken(access_token, token_secret);
+    let current_time = ("" + new Date()).substring(0, 21);
+    let new_status = current_time + " " + message.url;
 
    cb.__call(
 		"statuses_update",
-		{"status": message.url},
+		{"status": new_status},
 		function (reply, rate, err) {
             console.log(reply);
             console.log(rate);
